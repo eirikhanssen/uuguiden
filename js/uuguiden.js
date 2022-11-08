521f5797c53e5145c429b19307fb51bb34ad5ebe
Eirik Hanssen <eirikhanssen79@gmail.com>
2022-11-08 12:13:12 +0100

    add js for selecting main filtering logic

--- js/uuguiden.js
+++ js/uuguiden.js
@@ -1,35 +1,54 @@
-var uuguiden = function(){
+var uuguiden = function () {
   console.log('uuguiden()');
-  var openRequestedDetails = function(){
+  var openRequestedDetails = function () {
     var location_hash = location.hash.split('?')[0];
 
-    if(location_hash != undefined && location_hash != '') {
+    if (location_hash != undefined && location_hash != '') {
       var requested_details = document.querySelector('details' + location_hash);
       if (requested_details != null) {
-        requested_details.setAttribute("open","");
+        requested_details.setAttribute("open", "");
       }
     }
   }
 
+  var openTargetDetails = function (e) {
+    // check if link target is a target details on the same page.
+    // set the open attribute if it is
 
-var openTargetDetails = function(e) {
-// check if link target is a target details on the same page.
-// set the open attribute if it is
+    var anchor = e.target;
+    var href = anchor.getAttribute("href");
+    console.log('anchor-href: ' + href);
 
-var anchor = e.target;
-var href = anchor.getAttribute("href");
-console.log('anchor-href: ' + href);
+    document.querySelector(href).setAttribute('open', 'open');
 
-document.querySelector(href).setAttribute('open','open');
+  }
+
+  var links = document.querySelectorAll('a');
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', openTargetDetails, false);
+  }
+
+  function add_clicker_fn_to_selector(selector, fn) {
+    let domnodes = document.querySelectorAll(selector);
+    for (let i = 0; i < domnodes.length; i++) {
+      domnodes[i].addEventListener('click', fn)
+    }
+  }
+
+  function apply_filter_main_buttons(e) {
+    target = e.target;
+    document.body.setAttribute("data-filter-main", target.getAttribute("data-filter"));
+  }
+  add_clicker_fn_to_selector("#filter-main button", apply_filter_main_buttons);
+
+
+  function apply_filter_main_radios() {
+    document.body.setAttribute("data-filter-main", document.querySelector('input[name="customizer-main"]:checked').value);
+  }
+  add_clicker_fn_to_selector("#filter-main-radios button", apply_filter_main_radios);
+
+  openRequestedDetails();
 
 }
 
-var links = document.querySelectorAll('a');
-for (var i=0; i<links.length; i++) {
-links[i].addEventListener('click',openTargetDetails,false);
-}
-
-openRequestedDetails();
-}
-
-window.addEventListener('load',uuguiden, false);
+window.addEventListener('load', uuguiden, false);
